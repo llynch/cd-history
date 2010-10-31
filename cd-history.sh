361c2236733cd919818264b74a69c93babe383c9
Louis Lynch <louis.lynch@gmail.com>
2010-10-31 14:46:10 -0400

    FIX not same result in greped and grepedcolor

--- cd-history.sh
+++ cd-history.sh
@@ -30,16 +30,15 @@ cdd() {
   # https://wiki.archlinux.org/index.php/Color_Bash_Prompt
   bldred=$(echo -e '\e[1;33m') # Red
   colorreset=$(echo -e '\e[0m')    # Text Reset
-  if [ "$*" == "" ]; then
-      grep="cat"
-  else
-      grep="grep -i --color -e '/'"
-  fi
+  grep="cat"
   i=31 # start in red
   for arg in $*;
   do
+      # prepare grep expression
+      grep="$grep | grep -i $arg"
+
+      # prepare a colored output
       color=$(echo -e "\e[1;${i}m")
-      grep="$grep -e $arg"
       cat $tmp | sed -n "s#$arg#$color&$colorreset#ip" > $cdhistorygrepedcolored
 
       cp $cdhistorygrepedcolored $tmp
@@ -47,14 +46,19 @@ cdd() {
   done
   cp $tmp $cdhistorygrepedcolored
 
-  cat $cdhistory | $grep > $cdhistorygreped
+  $DEBUG && echo "grep expression: $grep"
+  eval "cat $cdhistory | $grep > $cdhistorygreped"
 
   $DEBUG && echo "filter done" 
   # get the number of results for expression
-  nbResults=$(cat $cdhistorygreped | wc -l)
+  nbResults=$(cat $cdhistorygrepedcolored | wc -l)
   if [ "$nbResults" == "1" ];
   then
 
+    $DEBUG && echo "filtered"
+    $DEBUG && cat $cdhistorygreped
+    $DEBUG && echo "filtered colored"
+    $DEBUG && cat $cdhistorygrepedcolored
     # only one result so cd to it directly
     \cd $(sed '1p' $cdhistorygreped)
 
@@ -68,6 +72,7 @@ cdd() {
 
       #show restults whith line number and mark current directory
       $DEBUG && echo "show results"
+      $DEBUG && cat $cdhistorygreped
       cat $cdhistorygrepedcolored | sed "s#^$(pwd)\$#\*\ &#g" | nl
 
     # ask the user for a number
